4ec52d9a803fd5e0d8299bd2212f030955e6729a
Juste Domeikiene <j.peckyte@gmail.com>
2023-02-10 21:32:06 +0200

    Variables and data types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Karlis!");
+        System.out.println("Hello Sheo Goes Tech");
+        //System.out.println("Hello second time");
+        /*
+        This is
+        Multi-line
+        Comment
+         */
     }
 }
