cee43bd8bc7e209d28d10a68ae693f01c259c62f
Juste Domeikiene <j.peckyte@gmail.com>
2023-02-24 21:19:45 +0200

    GuessNumber, HW5

---
+++ src/homeworks/my.java
@@ -0,0 +1,35 @@
+package homeworks.HW4;
+
+import java.util.Scanner;
+
+public class my {
+    public static void main(String[] args) {
+        /*     Write an application, that will read two numbers from user
+        (of type int) and will print true, if both numbers are the
+        same sign (both are positive, or both are negative), or false
+        otherwise.
+
+        If at least one of given numbers is equal to 0, your application
+        should print false.*/
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter first number");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Please enter second number");
+        int num2 = scanner.nextInt();
+
+        int intValue = 0;
+        boolean boolValue;
+
+        if(intValue >=1){
+            boolValue = true;
+        } else{
+            boolValue = false;
+        }
+
+
+
+
+    }
+}
