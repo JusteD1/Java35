35a482d35f8a061f13f8d16465a3df62d7b649ac
Juste Domeikiene <j.peckyte@gmail.com>
2023-03-10 21:05:36 +0200

    DateAndTimeExamples, RegularExpression, HW10, HW11, HW9

--- src/homeworks/HW9.java
+++ src/homeworks/HW9.java
@@ -1,7 +1,30 @@
 package homeworks;
 
+import java.util.Scanner;
+
 public class HW9 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Please enter Christmas tree size");
+        int treeSize = scanner.nextInt();
+
+        for (int i = 1; i <= treeSize; i++) {
+            for (int star = treeSize - i; star > 0; star--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        } for (int i = 1; i <= treeSize-1; i++){
+            System.out.print(" ");
+        }
+        for (int j= treeSize-3; j > 0; j--){
+            System.out.print(" ");
+        } for (int k= 1; k > 0; k--){
+            System.out.print(" # ");
+        }
+        System.out.println();
     }
 }
